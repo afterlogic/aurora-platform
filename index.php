21609ed65ed4b8aa2bd41d8b91e9d151b3b30319
Vasil Sokolov <vasil@afterlogic.com>
2023-05-05 10:25:00 +0200

    added CORS related rules

--- index.php
+++ index.php
@@ -16,6 +16,22 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Credentials: true');
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+		// may also be using PUT, PATCH, HEAD etc
+		header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+	}
+
+	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+		header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+	}
+
+	exit(0);
+}
+
 if (isset($_GET['dbg']) || isset($_POST['dbg']))
 {
 	ini_set('display_errors', 1);
