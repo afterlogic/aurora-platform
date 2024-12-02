419370ad32c644a6d950a54d403c5bc5a6c76793
Vasil Sokolov <vasil.sokolov@gmail.com>
2024-12-02 12:14:17 +0200

    Removed CORS preflight request handler

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * @copyright Copyright (c) 2017, Afterlogic Corp.
+ * @copyright Copyright (c) 2024, Afterlogic Corp.
  * @license AGPL-3.0
  *
  * This code is free software: you can redistribute it and/or modify
@@ -16,28 +16,14 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 
-header("Access-Control-Allow-Origin: *");
-header('Access-Control-Allow-Credentials: true');
-
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
-		// may also be using PUT, PATCH, HEAD etc
-		header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-	}
-
-	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
-		header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-	}
-
-	exit(0);
-}
-
-if (isset($_GET['dbg']) || isset($_POST['dbg']))
-{
+if (isset($_GET['dbg']) || isset($_POST['dbg'])) {
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
 }
 include_once 'system/autoload.php';
+
+\Aurora\System\Api::UseDbLogs(true);
+\Aurora\System\Application::UseDbLogs();
 
 \Aurora\System\Application::Start();
